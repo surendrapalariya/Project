a8ce16ffcd2709ac90906f13aac4dbb9e8143d59
surendra <surendrapalariya@yahoo.com>
2017-09-20 23:17:41 +0530

    commit to local repo

---
+++ language-hooks-demo/src/main/java/com/test/language/hooks/CustomLanguageComponent.java
@@ -0,0 +1,30 @@
+package com.test.language.hooks;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+@Component(
+ property = { "language.id=en_US" }, 
+ service = ResourceBundle.class
+)
+
+public class CustomLanguageComponent  extends ResourceBundle  {
+	
+	ResourceBundle bundle = ResourceBundle.getBundle("content.Language", UTF8Control.INSTANCE);
+
+	@Override
+	protected Object handleGetObject(String key) {
+		System.out.println("getting key=="+key);
+		return bundle.getObject(key);
+	}
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return bundle.getKeys();
+	}
+
+}
